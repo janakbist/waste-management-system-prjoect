2208d0b47e53843dc53f5262790746dcd025d40e
Janak  bista <52617626+janakbist@users.noreply.github.com>
2021-02-04 19:01:50 +0545

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const content = <img src="rec.jpg"alt = "plant grown in waste"></img>
+const div = document.getElementById('root');
+ReactDOM.render(content,div);
